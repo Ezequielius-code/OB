5589519e3e42a93461d88709b4499028c3bcd34d
unknown <bruttoezequiel@gmail.com>
2022-02-28 09:58:58 -0300

    DataTypes

---
+++ Introducción a la programación/01-TiposDeDatos/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte varbyte = 15;
+        short varshort = 8;
+        int varint = 14;
+        long varlong = 3545868;
+        float temperature = 37.5F;
+        double currency = 109.88;
+        System.out.println(varbyte);
+
+        char myChar = '\u2764';
+        System.out.println(myChar);
+
+        //Array declaration
+        int myArray[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        //myArray = new int[10];
+
+        List<String> names = new ArrayList<>();
+    }
+}
