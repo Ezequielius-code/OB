7fc70a80765be2aed9cd2b64a6ff78c389c0e86e
unknown <bruttoezequiel@gmail.com>
2022-03-03 18:04:57 -0300

    DataTypes

---
+++ JavaBásico/variables-tipos-de-datos/src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //tipo identificador = 30;
+
+        //tipo identificador;
+        //identificador = 30;
+
+        //TIPOS DE DATOS PRIMITIVOS. (NO SON NULLEABLES).
+
+        //Enteros
+        byte number1 = 1; // 1 byte
+        short number2 = 2; // 2 bytes
+        int number3 = 3; // 4 bytes
+        long number4 = 4l; // 8 bytes
+
+        //Punto flotante
+        float decimal1 = 4.9f;
+        double decimal2 = 9.99d;
+
+        //Caracter
+        char character1 = 'a';
+
+        //Boolean
+        boolean one = true;
+        boolean zero = false;
+
+        //EL String NO ES UN TIPO DE DATO PRIMITIVO
+        //Strings
+        String name = "Gerardo";
+        String surname = "Gómez";
+
+        //TIPOS DE DATOS ENVOLTORIO. (SON CLASES)
+
+        Integer numberInt = null;
+        Long numberLong = 2L;
+    }
+}
