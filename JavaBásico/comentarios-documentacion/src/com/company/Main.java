2bf1079cccdd20e61ea590393a3fdd4baee187ff
unknown <bruttoezequiel@gmail.com>
2022-03-03 17:49:10 -0300

    Documentación

---
+++ JavaBásico/comentarios-documentacion/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+/**
+ * Clase principal punto de entrada aplicación
+ *
+ * @author Ezequiel
+ */
+public class Main {
+    /**
+     * Documentación
+     * Función principal punto de entrada a la aplicación
+     * @param args argumentos a recibir
+     * @since 1.0
+     */
+    public static void main(String[] args) {
+	// Comentario de una línea.
+        /*Comentario de varias líneas.*/
+    }
+}
