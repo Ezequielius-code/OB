90c258cd305457390fe25683b17202b11fd8cde4
unknown <bruttoezequiel@gmail.com>
2022-03-05 14:04:21 -0300

    FirstSessionCommit

---
+++ JavaBásico/02-Sintaxis/src/com/company/Types.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Types {
+
+    public static void main(String[] args) {
+
+        //1. Numerics
+            //1.1 Integers
+            byte variable1 = 4;
+            short variable2 = 10;
+            int variable3 = 30;
+            long variable4 = 100;
+            //1.2 Decimal & Floating point
+            float variable5 = 5.5f;
+            double variable6 = 10.5;
+
+        //2. Booleans
+            boolean variable7 = false;
+            boolean variable8 = true;
+
+        //3. Text
+            char variable9 = 'a';
+            String variable10 = "Lorem ipsum dolor sit amet...";
+    }
+}
